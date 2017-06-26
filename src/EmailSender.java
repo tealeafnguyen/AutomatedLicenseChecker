e2f45fc74c7d0d25067b60bb19660c2879bc27cc
tealeafnguyen <tlnguyen1@cpp.edu>
2017-06-25 19:31:02 -0700

    Update EmailSender.java

--- src/EmailSender.java
+++ src/EmailSender.java
@@ -8,38 +8,45 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class EmailSender {
-	
-	public static void send(String messageBlock) {
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
 
-		Session session = Session.getDefaultInstance(props,
-			new javax.mail.Authenticator() {
+	public static void send(String messageBlock) {
+
+		if(messageBlock.compareTo("") > 0){
+			Properties props = new Properties();
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.socketFactory.port", "465");
+			props.put("mail.smtp.socketFactory.class",
+					"javax.net.ssl.SSLSocketFactory");
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.port", "465");
+
+			Session session = Session.getDefaultInstance(props,
+					new javax.mail.Authenticator() {
 				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication("account","password");
+					return new PasswordAuthentication("name","password");
 				}
 			});
 
-		try {
+			try {
 
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("name@email.com"));
-			message.setRecipients(Message.RecipientType.TO,
-					InternetAddress.parse("name@email.com"));
-			message.setSubject("Testing Subject");
-			message.setText(messageBlock);
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress("name@email.com"));
+				message.setRecipients(Message.RecipientType.TO,
+						InternetAddress.parse("name@email.com"));
+				message.setSubject("Testing Subject");
+				message.setText(messageBlock);
 
-			Transport.send(message);
+				Transport.send(message);
 
-			System.out.println("Done");
+				System.out.println("Done");
 
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
+			} catch (MessagingException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		else{
+			System.out.println("No License Expiring Soon");
+			System.exit(0);
 		}
 	}
 }
