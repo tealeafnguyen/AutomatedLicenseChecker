73429ae7cf4a4897fb35180d5b276c8871d037bf
tealeafnguyen <tlnguyen1@cpp.edu>
2017-06-25 19:30:16 -0700

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 
 public class Controller {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		if(args.length <= 0){
 			System.out.println("No file name provided");
@@ -9,7 +10,8 @@ public class Controller {
 		}
 		else{
 			String fileName = args[0];
-			System.out.println(fileName);
+			FileReader fr = new FileReader();
+			fr.firstSheet(fileName);
 		}
 
 		//		DayCounter dc = new DayCounter();
